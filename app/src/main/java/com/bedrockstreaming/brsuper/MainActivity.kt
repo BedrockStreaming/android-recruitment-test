0fa8b360748b39d74a68da11e0c1fb4e79175fe6
Baptiste Candellier <baptiste.candellier@bedrockstreaming.com>
2023-06-23 16:09:11 +0200

    Retrieve window size class

--- app/src/main/java/com/bedrockstreaming/brsuper/MainActivity.kt
+++ app/src/main/java/com/bedrockstreaming/brsuper/MainActivity.kt
@@ -3,18 +3,22 @@ package com.bedrockstreaming.brsuper
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.core.view.WindowCompat
 import com.bedrockstreaming.brsuper.feature.list.presentation.ListScreen
 import com.google.accompanist.themeadapter.material3.Mdc3Theme
 
 class MainActivity : AppCompatActivity() {
 
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
             Mdc3Theme {
+                val windowSizeClass = calculateWindowSizeClass(this@MainActivity)
                 ListScreen()
             }
         }
