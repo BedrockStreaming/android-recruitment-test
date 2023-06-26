33370a605b81af7d5cc1b630276638723d3d9aea
Baptiste Candellier <baptiste.candellier@bedrockstreaming.com>
2023-06-26 12:05:01 +0200

    Add missing inject

--- app/src/main/java/com/bedrockstreaming/brsuper/feature/detail/inject/HeroDetailsModule.kt
+++ app/src/main/java/com/bedrockstreaming/brsuper/feature/detail/inject/HeroDetailsModule.kt
@@ -1,9 +1,11 @@
 package com.bedrockstreaming.brsuper.feature.detail.inject
 
+import com.bedrockstreaming.brsuper.component.hero.domain.GetHeroBiographyUseCase
 import com.bedrockstreaming.brsuper.feature.detail.presentation.DetailsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val HeroDetailsModule = module {
-    viewModel { DetailsViewModel() }
+    viewModel { DetailsViewModel(get()) }
+    single { GetHeroBiographyUseCase(get()) }
 }
