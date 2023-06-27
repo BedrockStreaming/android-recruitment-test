5fd2ad8ff8efa7193d363e532e5c2911ecc56171
Baptiste Candellier <baptiste.candellier@bedrockstreaming.com>
2023-06-27 13:21:32 +0200

    Fix view model lifecycle issue

--- app/src/main/java/com/bedrockstreaming/brsuper/feature/navigation/inject/NavigationModule.kt
+++ app/src/main/java/com/bedrockstreaming/brsuper/feature/navigation/inject/NavigationModule.kt
@@ -1,8 +1,9 @@
 package com.bedrockstreaming.brsuper.feature.navigation.inject
 
 import com.bedrockstreaming.brsuper.feature.navigation.presentation.MainNavigationViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val NavigationModule = module {
-    single { MainNavigationViewModel() }
+    viewModel { MainNavigationViewModel() }
 }
